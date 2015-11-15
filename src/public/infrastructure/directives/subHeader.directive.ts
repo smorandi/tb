4c2a49c8c95883cb5b0dce167772268e9136e91e
mizollin <michael.zollinger@bluewin.ch>
2015-11-15 12:18:15 +0100

    basket,order,subheader,footer

--- src/public/infrastructure/directives/subHeader.directive.ts
+++ src/public/infrastructure/directives/subHeader.directive.ts
@@ -5,7 +5,7 @@
 module directives {
     export function SubHeader(subHeaderService:services.SubHeaderService):ng.IDirective {
         return {
-            templateUrl: "components/header/subHeader.html",
+            templateUrl: "components/header/subHeader1.html",
             link: (scope:any) => {
                 scope.subTitle = subHeaderService.getSubTitle();
                 scope.subTitleTxt = subHeaderService.getSubTitleTxt();
